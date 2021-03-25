828694d13afcf199c3d66a43eb8808c9c276d40a
samuel <samuelguzmanajala@gamail.com>
2021-03-25 09:49:15 +0100

    main casi acabado

--- my_grep.c
+++ my_grep.c
@@ -8,24 +8,41 @@
 #include <fcntl.h>
 #define MAXSIZE 500
 
-int getLine(chat *pattern, FILE *file){
+int getLine(char *pattern, FILE *file){
     int numberOfMatches=0;
     int i=0;
     char archivo[500];
     int line=1;
 
-    while(fgets(archivo,500,file) != null){
-        if(strstr(archivo,pattern) != null){
-            printf("match on the following line: /n", line);
+    while(fgets(archivo,500,file) != NULL){
+        if(strstr(archivo,pattern) != NULL){
+            printf("match on the following line: %i", line);
+            printf("/n %s",archivo);
         }
         line++;
     }
 
 }
 
-main(int argc, char* argv[])
+int main(int argc, char* argv[])
 {
 
+    if(argc<3){
+        printf("To use grep command you need two arguments, the pattern and the file where you want to search");
+    }
+
+    char *pattern = argv[2];
+    FILE *file= fopen(argv[3], "r+");
+
+    if(file==NULL){
+        printf("can't read the file /n");
+    }else{
+        printf("leido correctamente/n");
+    }
+
+
+
+    getLine(pattern,file);
 
 
 }
